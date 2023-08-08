119ef5adb3675d20fd378f709442a3ba4ac0fd06
Braja <brajasundar14@gmail.com>
2023-08-08 07:56:06 +0530

    Data Validation is in progress

--- demo.py
+++ demo.py
@@ -1,12 +1,16 @@
 from housing.pipeline.pipeline import Pipeline
+from housing.logger import logging
+from housing.config.configuration import Configuration
 
 def main():
     try:
-        pipeline = Pipeline()
-        pipeline.run_pipeline()
-    
+        #pipeline = Pipeline()
+        #pipeline.run_pipeline()
+        data_validation_config = Configuration().get_data_validation_config()
+        print(data_validation_config)    
     except Exception as e:
-        print("ssssss")
+        logging.error(f"{e}")
+        print(e)
 
 if __name__=="__main__":
     main()
--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -4,7 +4,7 @@ DataIngestionConfig = namedtuple("DataIngestionConfig",
 ["dataset_download_url","tgz_download_url","raw_data_dir","ingested_train_dir","ingested_test_dir"])
 
 DataValidationConfig = namedtuple("DataValidationConfig",
-["schema_file_path"])
+["schema_file_path","report_file_path","report_page_file_path"])
 
 DataTransformationConfig = namedtuple("DataTransformationConfig",
 ["add_bedroom_per_room","transformed_train_dir","transformed_test_dir","preprocessed_object_file_path"])
